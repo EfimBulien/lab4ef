38562dc3632271b336f1a2fa2028317249b53acb
EfimBulien <isip_e.m.bulien@mpt.ru>
2024-03-19 19:01:56 +0300

    first commit

---
+++ CarsEditorWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace laba2ef
+{
+    public partial class CarsEditorWindow : Window
+    {
+        private lab1Entities lab1Entities = new lab1Entities();
+        public CarsEditorWindow()
+        {
+            InitializeComponent();
+            CarsDataGrid.ItemsSource = lab1Entities.cars.ToList();
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            CarsDataGrid.ItemsSource = lab1Entities.cars.ToList().Where(item => item.model.Contains(searchBox.Text));
+            ResetTextBoxes();
+        }
+
+        private void numberButton_Click(object sender, RoutedEventArgs e)
+        {
+            var searchtext = searchBox.Text;
+            searchtext = searchtext.ToUpper();
+            CarsDataGrid.ItemsSource = lab1Entities.cars.ToList().Where(item => item.number.Contains(searchtext));
+            ResetTextBoxes();
+        }
+
+        private void colorButton_Click(object sender, RoutedEventArgs e)
+        {
+            var searchtext = searchBox.Text;
+            searchtext = searchtext.ToLower();
+            CarsDataGrid.ItemsSource = lab1Entities.cars.ToList().Where(item => item.color.Contains(searchtext));
+            ResetTextBoxes();
+        }
+
+        private void ResetTextBoxes()
+        {
+            searchBox.Text = string.Empty;
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            CarsDataGrid.ItemsSource = lab1Entities.cars.ToList();
+            ResetTextBoxes();
+        }
+
+        /*
+        
+        private void createButton_Click(object sender, RoutedEventArgs e)
+        {
+           cars car = new cars();
+           car.model = modelBox.Text;
+           car.number = numberBox.Text;
+           car.color = colorBox.Text;
+           lab1Entities.cars.Add(car);
+           SaveChanges();
+           ResetTextBoxes();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+           if (CarsDataGrid != null)
+           {
+               var selected = CarsDataGrid.SelectedItem as cars;
+               selected.model = modelBox.Text;
+               selected.number = numberBox.Text;
+               selected.color = colorBox.Text;
+               SaveChanges();
+           }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+           if(CarsDataGrid != null)
+           {
+               lab1Entities.cars.Remove(CarsDataGrid.SelectedItem as cars);
+               SaveChanges();
+               ResetTextBoxes();
+           }
+        }
+
+        private void SaveChanges()
+        {
+           lab1Entities.SaveChanges();
+           CarsDataGrid.ItemsSource = lab1Entities.cars.ToList();
+        }
+
+        private void ResetTextBoxes()
+        {
+           modelBox.Text = string.Empty;
+           numberBox.Text = string.Empty;
+           colorBox.Text = string.Empty;
+        }
+
+        */
+    }
+}
